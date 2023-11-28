1f066bdb23c0374b3770db858a5017696b377527
filippogiovannoni <filippogiovannonii@gmail.com>
2023-11-28 11:19:49 +0100

    add pseudocode for dice game

--- js/script.js
+++ js/script.js
@@ -23,3 +23,30 @@ if (mail_list.includes(mail)) {
     //Viene mostrato l'alert con l'accesso negato
     alert('Accesso negato.✋🛑');
 }
+
+// Gioco dei Dadi
+// Pseudocodice
+
+
+// Salvo in una variabile l'array con i valori dei dadi da 1 a 6
+
+// Salvo in una variabile il numero random del dado per il giocatore
+
+// Salvo in una variabile il numero random del dado per il computer
+
+// Salvo in una variabile il pulsante per lanciare il dado del giocatore
+
+// Salvo in una variabile il pulsante per lanciare il dado del computer
+
+//Quando viene cliccato sul pulsante del giocatore, viene lanciato il dado e viene stampato il valore in pagina
+
+//Quando viene cliccato sul pulsante del computer, viene lanciato il dado e viene stampato il valore in pagina
+
+    // Se il dado del giocatore è superiore a quello del computer
+        // Viene stampata in pagina la vittoria del giocatore
+
+    // Altrimenti se il dado del giocatore è inferiore a quello del computer
+        // Viene stampata in pagina la vittoria del computer
+
+    // Altrimenti il dado del giocatore e del computer sono uguali
+        // Viene stampato in pagina il pareggio
